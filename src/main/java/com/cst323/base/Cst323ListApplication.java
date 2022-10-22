4a57a28827f609cab4271bc1a251f0f461cd0be6
Kekoa Kubli <kkubli3246@gmail.com>
2022-10-22 16:27:40 -0700

    Update Cst323ListApplication.java

--- src/main/java/com/cst323/base/Cst323ListApplication.java
+++ src/main/java/com/cst323/base/Cst323ListApplication.java
@@ -3,11 +3,20 @@ package com.cst323.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.*;
+
 @SpringBootApplication
 public class Cst323ListApplication {
+	
+	static Logger log = LoggerFactory.getLogger(Cst323ListApplication.class);
+	
 
 	public static void main(String[] args) {
+		
+		log.info("Started Application");
 		SpringApplication.run(Cst323ListApplication.class, args);
+		
+
 	}
 
 }
